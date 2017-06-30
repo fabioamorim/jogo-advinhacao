081a7e6ec362cd4cf2178c8bfe655754d04d5538
Fábio Amorim <batista.amorim@hotmail.com>
2017-06-30 17:30:06 -0300

    atualizacao

--- jogo_advinhacao.c
+++ jogo_advinhacao.c
@@ -14,31 +14,26 @@ int main(){
 
 	for(int i=1;i<=NUMERO_DE_TENTATIVAS;i++){
 
-		printf("tentativa %d de %d\n",i,NUMERO_DE_TENTATIVAS);
-
+		printf("\ntentativa %d de %d\n",i,NUMERO_DE_TENTATIVAS);
 		printf("Qual e' o seu chute? ");
 		scanf("%d",&chute);
 		printf("\nSeu chute foi %d\n", chute);
 
-		int acertou = (chute == numSecreto);// Se verdadeiro retorna 1 se falso retorna 0.
-
+		int acertou = chute == numSecreto;// Se verdadeiro retorna 1 se falso retorna 0.
+		int maior = chute > numSecreto;
+		int menor = chute < numSecreto;
 		if(acertou){
 			printf("Parabens, voce acertou!\n");
 			printf("Voce e' um otimo jogador, tente de novo!\n");
 			break;
-		}else{
-
-			printf("Que pena, voce errou. ");
-
-			int maior = chute > numSecreto; 
-
-			if(maior){
-				printf("Seu chute e' maior que o numero secreto\n");
-			}else{
-				printf("Seu chute e' menor que o numero secreto\n");
-			}
+		}else if(maior){
+			printf("Seu chute e' maior que o numero secreto\n");
+		
+		}else if(menor){
+			printf("Seu chute e' menor que o numero secreto\n");
+		}
 	}		
-}
+
 
 
 	printf("Fim de jogo!");
