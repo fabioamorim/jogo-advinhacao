8b10b66dd9b85a286fb865443ff688e9b6dbc768
Fábio Amorim <batista.amorim@hotmail.com>
2017-07-11 22:07:57 -0300

    atualizao

--- jogo_advinhacao.c
+++ jogo_advinhacao.c
@@ -10,16 +10,39 @@ int main(){
 	printf("**********************************\n");
 
 	int segundos = time(0);// pega a data em segundos desde 01/abril/1970 até o momento.
-	srand(segundos);// gerando a semente para o número radômico.
-
+	srand(segundos);// gerando a semente para o número randômico.
+	int nivel;
+	int i=0;
+	int acertou = 0;
 	int numGrande= rand();
-
+	int numDeTentativas;
 	int numSecreto = numGrande % 100;
 	int chute = 0;
 	int tentativa = 1;
 	double pontos = 1000;
 
-	while(1){
+	printf("Qual o nivel de dificuldade pretende jogar?\n");
+	printf("(1) Facil (2) Medio (3) Dificil\n\n");
+	printf("Escolha: ");
+	scanf("%d",&nivel);
+
+	switch(nivel){
+		case 1:
+			numDeTentativas = 20;
+			break;
+		case 2:
+			numDeTentativas = 10;
+			break;
+		case 3:
+			numDeTentativas = 5;
+		
+		default:
+			printf("Nivel invalido!Ira para o facil!");
+			numDeTentativas = 20;
+			break;
+	}
+
+	for(i = 1;i <= numDeTentativas;i++){
 
 		printf("\nTentativa numero:%d ",tentativa);
 		printf("Qual e' o seu chute? ");
@@ -31,11 +54,10 @@ int main(){
 			continue;
 		}
 
-		int acertou = chute == numSecreto;// Se verdadeiro retorna 1 se falso retorna 0.
+		acertou = chute == numSecreto;// Se verdadeiro retorna 1 se falso retorna 0.
 		int maior = chute > numSecreto;
 		int menor = chute < numSecreto;
 		if(acertou){
-			printf("Voce e' um otimo jogador, tente de novo!\n");
 			break;
 		}else if(maior){
 			printf("Seu chute e' maior que o numero secreto\n");
@@ -51,8 +73,15 @@ int main(){
 
 	}	
 
-	printf("Seu score: %.1f\n",pontos);
-	printf("Parabe'ns, voce acertou na tentativa numero:%d\n",tentativa);
-	printf("Fim de jogo!");
+	if(acertou){
+		printf("Voce ganhou!\n");
+		printf("Seu score: %.1f\n",pontos);
+		printf("Parabe'ns, voce acertou na tentativa numero:%d\n",tentativa);
+		printf("Fim de jogo!");
+	}else{
+		printf("Voce perdeu!Tente de novo.");
+	}
+
+	
 
 }
